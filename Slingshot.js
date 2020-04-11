4ec0e374ffc8f5b3022338d6918940a4dfefdef8
Suryansh2010 <suryansh2010@icloud.com>
2020-04-11 16:16:20 +0400

    s

--- Slingshot.js
+++ Slingshot.js
@@ -20,6 +20,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
     display(){
         image(this.sling1,200,20);
         image(this.sling2,175,20);
